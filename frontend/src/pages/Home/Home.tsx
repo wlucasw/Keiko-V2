b7c55ab19a937ce48b26216d664e476f7696583a
Francois Hendriks <francoishendriks96@gmail.com>
2022-02-08 17:17:22 +0100

    chore: remove first exercise from master

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,17 +1,10 @@
 import styles from "./Home.module.css"
 
 export const Home = () => {
-  const pokemon = "Carapuce"
-
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <div>
-        <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png' />
-        <p>Name: {pokemon}</p>
-        <p>Number: 7</p>
-      </div>
     </div>
   )
 }
