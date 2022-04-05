e760aafdd9d7ffde02232b5ffdf173d0d2ee8be0
Francois Hendriks <francoishendriks96@gmail.com>
2022-04-05 22:12:46 +0200

    feat: add pokemons to nestjs app

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/pokemons')
+  getPokemons() {
+    return this.appService.getPokemonLists();
+  }
+
+  @Get('/pokemon/:id')
+  findOne(@Param() params: { id: number }) {
+    return this.appService.getPokemonFromId(params.id);
   }
 }
