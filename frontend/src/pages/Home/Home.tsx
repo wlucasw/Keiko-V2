dbe5cb42af8854064a4381a5b77ebd842161fd31
wlucaswPC <lucas.panier@ext.bpifrance.fr>
2023-11-06 11:26:57 +0100

    add nex pokemon

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,6 +5,13 @@ export const Home = () => {
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <div>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/445.png"
+          alt="pokemon carcha"
+        ></img>
+        <p>name : carcha</p>
+      </div>
     </div>
   )
 }
